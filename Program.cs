8e9919c866f163712cef860d29d757231f9192e6
Atsert2 <lia.jp2002@gmai.com>
2025-09-17 19:55:52 -0300

    Versão de Luiza Freire

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Que belo que é");Thread.Sleep(1000);
+Console.WriteLine("não pensar ao ver um raio:");Thread.Sleep(2000);
+Console.WriteLine("“A vida é fugaz”");Thread.Sleep(3000);
